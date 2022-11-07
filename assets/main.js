c49acf9c72bf96d947cdb338afe2686256debebf
longGCO <ngoclong3066@gmail.com>
2022-11-07 21:44:19 +0700

    l

---
+++ assets/main.js
@@ -0,0 +1,60 @@
+// Bài 5
+// var n = parseInt(prompt("Nhập số cần kiểm tra:"));
+// var index = 0;
+// document.write("Số nhập vào:" + n) + "</br>";
+// while (index < n) {
+//   // Nếu số chẵn vào
+//   if (index % 2 == 0) {
+//     document.write("</br>" + index + "</br>");
+//   }
+
+//   // tăng biến đếm
+//   index++;
+// }
+
+// Bài 4
+// const arr = [0, 2, -4, 8];
+// let sum = 0;
+// // Vòng lập for of
+// for (const a of arr) {
+//   if (a >= 0) {
+//     sum += a;
+//   }
+// }
+
+// console.log("Tổng:" + sum);
+
+// Bài 3
+// var n = parseInt(prompt("Nhập số cần kiểm tra:"));
+// var index = 1;
+// var fact = 1;
+// while (index <= n) {
+//   fact = fact * index;
+//   index++;
+// }
+// document.write("Đầu ra được in trên màn hình n:" + n + "=>" + fact);
+
+// Bài 2
+// var a = parseInt(prompt("Nhập a:"));
+// var b = parseInt(prompt("Nhập b:"));
+// var c = parseInt(prompt("Nhập c:"));
+// var max = a;
+// if (b > max) {
+//   max = b;
+// }
+// if (c > max) {
+//   max = c;
+// }
+// document.write("Số lớn nhất  trong 3 số a,b,c là:" + max);
+
+// Bài 1
+// var a;
+// a = prompt("Nhập vào số a", "0");
+
+// a = parseInt(a);
+
+// if (a % 2 == 0) {
+//   document.write("a là số chẵn");
+// } else {
+//   document.write("a là số lẽ");
+// }
